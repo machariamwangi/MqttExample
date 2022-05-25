4a682d7be18fc0a849d63a019cdecd177dc3ed48
ammwangi <elijah.mwangi@kamsware.com>
2022-05-25 09:47:55 +0300

    Add project files.

---
+++ MqttExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace MqttExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string UserName = "guest";
+            string Password = "guest";
+            string HostName = "localhost";
+
+            //Main entry point to the RabbitMQ .NET AMQP client
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory()
+            {
+                UserName = UserName,
+                Password = Password,
+                HostName = HostName
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+
+            Console.WriteLine("Creating Exchange");
+            // Create Exchange
+            model.ExchangeDeclare("demoExchange", ExchangeType.Direct);
+
+            //create QUEUE
+            Console.WriteLine("Creating Queue");
+            model.QueueDeclare("demoqueue", true, false, false, null);
+
+            //Bind Queue to exchange
+            Console.WriteLine("Creating Binding");
+            model.QueueBind("demoqueue", "demoExchange", "directexchange_key");
+
+            Console.ReadLine();
+
+        }
+    }
+}
